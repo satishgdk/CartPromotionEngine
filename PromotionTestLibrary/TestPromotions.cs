6c9375e42f882033b6bd69fef978fbdc95b4ff46
ksatishvja <ksatishvja@gmail.com>
2020-08-25 12:47:14 +0530

    Adding Test Framework in Library for TDD approach

---
+++ PromotionTestLibrary/TestPromotions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit;
+using NUnit.Framework;
+
+namespace PromotionTestLibrary
+{
+    [TestFixture]
+    public class TestPromotions
+    {
+
+        [Test]
+        public void TestScenario1()
+        {
+            //Arrange
+            //Act
+            //Assert
+
+            Assert.IsTrue(true);
+        }
+    }
+}
